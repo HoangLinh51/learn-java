8402eed41683ebf01fc836c63849424de7e1e068
HoangLinh51 <linhle5112002@gmail.com>
2024-12-29 16:11:30 +0700

    first commit

---
+++ src/main/java/com/javaproject/project_backend_spring/ProjectBackendSpringApplication.java
@@ -0,0 +1,12 @@
+package com.javaproject.project_backend_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ProjectBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBackendSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaproject/project_backend_spring/ProjectBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.project_backend_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
